c7b21920b600e96048a5b76579ef4947394aefeb
Cherylzyy <31760458+Cherylzyy@users.noreply.github.com>
2024-08-19 18:59:10 +0800

    Add files via upload

---
+++ learn/learn/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  learn
+//
+//  Created by zoezyyzhang on 2024/8/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ learn/learn/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  learn
+//
+//  Created by zoezyyzhang on 2024/8/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+@end
